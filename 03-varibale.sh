7ac1a714c1a45a42061c9771b65e7b4c55b7bbe5
LSETProject2024 <lsetdevops2024@gmail.com>
2024-08-01 15:05:19 +0100

    varibale example

---
+++ 03-varibale.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of variable
+person1=kalpana #no space between = and name
+person2=suneel
+
+#referring variable
+echo "$person1 : Hello $person2, How are you doing?"
+echo "$person2 : Hi $person1, Iam fine how are you doing?"
+echo "$person1 : I am fine too. how is your work?"
+echo "$person2 : Not bad, I am planning to upgrade to devops"
